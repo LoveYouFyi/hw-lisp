8f5ff5c04316ac46f589e0526adc221f9ae4641b
NEH <erik@bodigiti.com>
2020-05-22 09:19:11 -0400

    master: Adds checkParens function that tests for equal number of opening and closing parens.

---
+++ index.js
@@ -0,0 +1,11 @@
+const checkParens = (string) => {
+  return !string.split("").reduce((a, c) => {
+    if (c === "(") return ++a
+    if (c === ")") return --a
+    return a
+  }, 0);
+}
+
+console.log(checkParens("( ( ( ( ) (( ) ) )"));
+console.log(checkParens("( ( ) )"));
+console.log(checkParens("( ( (()()) ) )"));
